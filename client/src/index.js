d71a542679b688629373eb998cef060852b580fa
Andrew Keiser <webdev410@gmail.com>
2021-11-03 15:27:48 -0700

    user login and basic template

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+import "./css/index.css";
 import App from "./App";
 
 ReactDOM.render(
